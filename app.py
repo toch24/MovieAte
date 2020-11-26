8e9343c5804f9d6d37933fef18e68100a0d1a20a
Tommy Chong <tc18cj@my.fsu.edu>
2020-11-25 19:50:50 -0500

    Edit and login change

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def postlogin():
     username = request.form['username']
     password = request.form['password']
 
-    c.execute("SELECT Username, Password FROM Users, Logins WHERE Users.Username = ? AND Logins.Password = ?", (username, password))
+    c.execute("SELECT Username, Password FROM Users WHERE Username = ? AND Password = ?", (username, password))
     data = c.fetchone()
 
     if data:
@@ -174,7 +174,7 @@ def postadd():
 def mymovies():
     c = conn().cursor()
     try:
-        c.execute("SELECT movieName, MovieYear, Director_name, Genre, Lead_Actor, roles, Rating, review, UserRating, url FROM Watched_Movies WHERE Username = ?", session['username'])
+        c.execute("SELECT movieName, MovieYear, Director_name, Genre, Lead_Actor, roles, Rating, review, UserRating, url, movieID FROM Watched_Movies WHERE Username = ?", session['username'])
         data = c.fetchall()
         c.close()
         return render_template('mymovies.html', rows = data, usr=session['username'] if 'username' in session else "null", is_log=session['logged'] if 'logged' in session else False)
@@ -184,6 +184,34 @@ def mymovies():
         return render_template('mymovies.html', usr=session['username'] if 'username' in session else "null", is_log=session['logged'] if 'logged' in session else False)
 
     
+@app.route('/edit', methods = ['GET', 'POST'])
+def edit():
+    id = request.form['id']
+    return render_template('edit.html', id = id,usr=session['username'] if 'username' in session else "null", is_log=session['logged'] if 'logged' in session else False)
+
+@app.route('/postedit', methods = ['GET', 'POST'])
+def postedit():
+    c = conn().cursor()
+    id = request.form['id']
+    password = request.form['password']
+    userrating = request.form['userrating']
+    review = request.form['review']
+
+    c.execute("SELECT Password FROM Users WHERE Username = ?", session['username'])
+    userpassword = c.fetchone()
+    print(userpassword)
+    if password == userpassword:
+        try:
+            c.execute("UPDATE Watched_Movies SET UserRating = ?, review = ? WHERE movieID = ?", userrating, review, id)
+            c.commit()
+            flash("Successfully Updated")
+            return redirect('/mymovies')
+        except:
+            flash("Unable to edit")
+            return redirect('/mymovies')
+    else:
+        flash("Password incorrect")
+        return redirect('/mymovies')
 
 @app.route('/register')
 def register():
@@ -196,18 +224,16 @@ def postregister():
     location = request.form['location']
     fname = request.form['fname']
     lname = request.form['lname']
-    email = request.form['email']
     password = request.form['password']
     cpassword = request.form['cpassword']
 
     if password == cpassword:
         try:
-            c.execute("INSERT INTO Users (Username, firstname, lastname, Locations) VALUES (?, ?, ?, ?)", username, fname, lname, location)
-            c.execute("INSERT INTO Logins (Email, Password) VALUES (?, ?)", email, password)
+            c.execute("INSERT INTO Users (Username, firstname, lastname, Locations, Password) VALUES (?, ?, ?, ?, ?)", username, fname, lname, location, password)
             flash('Successfully registered')
             c.commit()
         except:
-            flash('User or/and email address already exists')
+            flash('User already exists')
             c.rollback()
     else:
         flash('Password does not meet confirm password')
